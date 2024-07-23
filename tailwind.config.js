af41ce2bd53253d00e95b0f18b9de54169f6eabe
zyadfallatah <zyadfallatah@gmail.com>
2024-07-23 08:23:43 +0300

    Project intialize

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-cyan": "hsl(179deg, 81%, 29%)",
+        "dark-grey-blue": "hsl(215deg, 19%, 25%)",
+        "pale-orange": "hsl(25deg, 94%, 86%%)",
+        "light-cream": "hsl(43deg, 78%, 98%)",
+        grey: "hsl(215deg, 5%, 54%)",
+      },
+    },
+  },
+  plugins: [],
+};
