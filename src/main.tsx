32ae2312a7bed9c17c6d6c4d0ad763ac7ad2ca3b
zyadfallatah <93343139+zyadfallatah@users.noreply.github.com>
2024-11-24 17:02:57 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ import "./index.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/frontend-mentor-challenge-10/" element={<MainLayout />}>
+    <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="about" element={<AboutPage />} />
       <Route path="create-your-plan" element={<CreateYourPlanPage />} />
